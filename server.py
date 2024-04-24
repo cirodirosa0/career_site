7126be39475890ee9ffe0714098af5c671d47924
ciro <cirodirosa0@gmail.com>
2024-04-24 09:27:16 -0400

    add plot functionality

--- server.py
+++ server.py
@@ -1,15 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, Response, request
+from testing import Testing
 
 app = Flask(__name__)
+test_source = Testing()
+
 
 @app.route("/")
 @app.route("/index.html")
 def home():
     return render_template('index.html')
 
+
 @app.route("/learn_more.html")
 def learn_more():
     return render_template('learn_more.html')
+
+
+@app.route('/plot.png')
+def plot():
+    threshold = request.args.get('threshold', default=0.1, type=float)
+    img = test_source.plot_me(threshold)
+    return Response(img.getvalue(), mimetype='image/png')
+
+@app.route('/update_plot')
+def update_plot():
+    threshold = request.args.get('threshold', default=0.1, type=float)
+    img = test_source.plot_me(threshold)
+    return Response(img.getvalue(), mimetype='image/png')
+
+@app.route("/testing.html")
+def test():
+    return render_template('testing.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- testing.py
+++ testing.py
@@ -1,8 +1,56 @@
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import io
 
-dotenv_path = '/home/cirodirosa0/career_site/.env'
-load_dotenv(dotenv_path)
 
-secret_key = os.getenv("SECRET_KEY")
-print(secret_key)
+class Testing:
+    def __init__(self):
+        self.test_data = pd.read_csv("./static/assets/test.csv")
+        self.train_data = pd.read_csv("./static/assets/train.csv")
+        self.train_dict = self.train_data.to_dict(orient="index")
+        self.test_dict = self.test_data.to_dict(orient="index")
+
+    def otherinfo(self):
+        dotenv_path = '/home/cirodirosa0/career_site/.env'
+        load_dotenv(dotenv_path)
+
+        secret_key = os.getenv("SECRET_KEY")
+        print(secret_key)
+
+    def plot_me(self, threshold, figsize=(5, 3.33)):
+        self.train_data['Sex'] = self.train_data['Sex'].replace({'male': 1, 'female': 0})
+        self.train_data['Embarked'] = self.train_data['Embarked'].replace({'S': 0, 'C': 1, 'Q': 2})
+
+        combined_data = pd.DataFrame({
+            'Combined_Column': self.train_data['Sex'] + self.train_data['Pclass'] + self.train_data['Embarked'],
+            'Survived': self.train_data['Survived']
+        })
+
+        exclude_columns = ['Name', 'Ticket', 'Cabin', 'Fare', 'Unnamed: 0']
+        # Filter out non-numeric columns
+        numeric_columns = self.train_data.select_dtypes(include=[np.number]).columns.tolist()
+        numeric_columns = [col for col in numeric_columns if col not in exclude_columns]
+
+        correlation_matrix = self.train_data[numeric_columns].corr()
+
+        # Exclude correlations below the threshold
+        correlation_matrix = correlation_matrix[correlation_matrix.abs() >= threshold]
+
+        # Set the size of the Matplotlib figure
+        fig, ax = plt.subplots(figsize=figsize)
+
+        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", ax=ax)
+        plt.xticks(rotation=45)
+        plt.title('Correlation Heatmap')
+
+        img = io.BytesIO()
+        plt.savefig(img, format='png', bbox_inches='tight')
+        img.seek(0)
+
+        plt.close()
+
+        return img
